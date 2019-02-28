55f742f2a800008ca9ef046ed2a3b637e590b84e
KingsleyChung <kingsleychung@outlook.com>
2019-03-01 01:18:58 +0800

    add two day match

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(paramsParser())
 
 router.get('/matchInfo', async ctx => {
   console.log(ctx.params.player)
-  result = await getMatchInfo(ctx.params.player)
+  result = await getRecentMatchInfo(ctx.params.player)
   ctx.body = result
 })
 
@@ -23,12 +23,25 @@ app.listen(3000, () => {
   console.log('Server is running at port 3000')
 })
 
-const getMatchInfo = async name => {
+
+const getRecentMatchInfo = async name => {
   let date = new Date()
   let timeOffset = date.getTimezoneOffset() / 60
-  console.log(timeOffset)
-  date = moment(date.getTime() + (timeOffset + 8) * 3600 * 1000).format('YYYY-MM-DD')
-  console.log(date)
+  date = convertToBeijingTime(date.getTime(), timeOffset, 'YYYY-MM-DD')
+  let today = moment(date).format('YYYY-MM-DD')
+  let todayMatchTime = moment(convertToBeijingTime(await getMatchInfoOfDate(name, today), timeOffset)).format('MM-DD HH:mm')
+  if (date.getHours() >= 18) {
+    let tomorrow = moment(date.getTime() + 24 * 3600 * 1000).format('YYYY-MM-DD')
+    let tomorrowMatchTime = moment(convertToBeijingTime(await getMatchInfoOfDate(name, tomorrow), timeOffset)).format('MM-DD HH:mm')
+    return todayMatchTime + '; ' + tomorrowMatchTime
+  } else {
+    let yesterday = moment(date.getTime() - 24 * 3600 * 1000).format('YYYY-MM-DD')
+    let yesterdayMatchTime = moment(convertToBeijingTime(await getMatchInfoOfDate(name, yesterday), timeOffset)).format('MM-DD HH:mm')
+    return yesterdayMatchTime + '\n' + todayMatchTime
+  }
+}
+
+const getMatchInfoOfDate = async (name, date) => {
   let url = `https://coric.top/zh/result/${date}`
 
   return new Promise((resolve, reject) => {
@@ -45,7 +58,6 @@ const getMatchInfo = async name => {
         var data = Buffer.concat(chunks, size);  
         var html = data.toString();
         var $ = cheerio.load(html); //cheerio模块开始处理 DOM处理
-        var targetMatch
         
         $('.cResultMatch').toArray().forEach(match => {
           scoreBarArray = $(match).find('tbody').children().toArray()
@@ -54,8 +66,7 @@ const getMatchInfo = async name => {
           });
           if (result) {
             time = $(match).find('.cResultMatchTime').text()
-            time = moment(parseInt(time) * 1000 + (timeOffset + 8) * 3600 * 1000).format('MM-DD HH:mm')
-            resolve(time)
+            resolve(parseInt(time) * 1000)
           }
         })
         resolve('没有比赛')
@@ -64,3 +75,6 @@ const getMatchInfo = async name => {
   })
 }
 
+const convertToBeijingTime = (time, timeOffset) => {
+  return new Date(time + (timeOffset + 8) * 3600 * 1000)
+}
